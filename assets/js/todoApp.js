ad95551738923d0c22e6fc5c1c5bc14b1cd854d0
asmobina <asmobina10@gmail.com>
2023-09-07 21:20:44 +0330

    add final todo js

--- assets/js/todoApp.js
+++ assets/js/todoApp.js
@@ -1,14 +1,13 @@
 const input = document.querySelector(".input-item")
-const addBtn = document.querySelector(".add")
 const todoUl = document.querySelector(".todo-ul")
-const clearBtn = document.querySelector(".clear")
+const inputBtn = document.querySelector(".input-btn")
+const filter = document.querySelectorAll(".all-btn button")
+//
 
-//isotop liberary
-
-var iso = new Isotope('.todo-ul', {
-    itemSelector: '.li-item',
-    layoutMode: 'fitRows'
-});
+inputBtn.innerHTML = `
+        <button class="add same" onclick="addtodo()">Add To List</button>
+        <button class="clear same" onclick="cleartodo()">Clear Todo List</button>
+`
 
 //model
 let todos = []
@@ -40,23 +39,60 @@ function deleteHandler(index) {
     todos.splice(index, 1)
     render(todos)
 }
+function editHandler(index) {
+    input.value = todos[index].name
 
+    inputBtn.innerHTML = `
+        <button class="add same " onclick="editTodo(${index})">edit To List</button>
+        <button class="clear same" onclick="cleartodo()">Clear Todo List</button>
+    `
+}
 
+function editTodo(index) {
 
+    inputBtn.innerHTML = `
+    <button class="add same " onclick="addtodo()">add To List</button>
+    <button class="clear same" onclick="cleartodo()">Clear Todo List</button>
+    `
+    todos[index].name = input.value
+    input.value = ""
+    render(todos)
+    input.focus()
+}
 //view
 function render(todos) {
     const result = todos.map((item, index) => {
         return `<li class="li-item">
-        <sapn class="${item.isComplete ? "incomplete" : "complete"}">${item.name}</sapn>
+        <sapn class="${item.isComplete ? "complete" : "incomplete"}">${item.name}</sapn>
         <div>
         <button class="same comp " onclick="compleletHandler(${index})">${item.isComplete ? "InCompletet" : "Complete"}</button>
-        <button class="same delete m-0" onclick="deleteHandler(${index})">Delete</button>
+        <button class="same delete" onclick="deleteHandler(${index})">Delete</button>
+        <button class="same edit" onclick="editHandler(${index})">Edit</button>
         </div> 
         </li>`
     })
+
     todoUl.innerHTML = result.join("")
 }
 
 //event
-addBtn.addEventListener("click", addtodo)
-clearBtn.addEventListener("click", cleartodo)
+filter.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("button.active").classList.remove("active")
+        btn.classList.add("active")
+        let btnId = btn.id
+        if (btnId == "all") {
+            render(todos)
+        } else if (btnId == "complete") {
+            const result = todos.filter(item => {
+                return item.isComplete == true
+            })
+            render(result)
+        } else if (btnId == "Active") {
+            const result = todos.filter(item => {
+                return item.isComplete == false
+            })
+            render(result)
+        }
+    })
+})
